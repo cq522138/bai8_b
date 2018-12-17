5e862c5edfee4afc859bd78d17eead5f7e7ad3ce
cq522138 <41883350+cq522138@users.noreply.github.com>
2018-12-17 09:05:43 +0700

    Add files via upload

---
+++ form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  profile = {
+    username: 'Bob',
+    email: 'abc@deg.com',
+    facebook: 'facebook.com',
+    twitter: 'twitter.com',
+    website: 'example.com',
+    tel: '1234-5678-90'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(profileForm) {
+    console.log('1');
+    // unhandle exception
+    // throw new Error('Something went wrong');
+  }
+
+}
